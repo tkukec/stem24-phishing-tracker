727d3c4b4c87cc0c3ace527c3fa0ccb718fb8545
Andrej Božić <abozic389@gmail.com>
2024-05-15 15:40:42 +0200

    Adding husky and tailwind.

---
+++ frontend/.prettierrc.js
@@ -0,0 +1,10 @@
+/** @type {import("prettier").Config} */ 
+const config = {
+  semi: true,
+  singleQuote: false,
+  printWidth: 100,
+  tabWidth: 4,
+  endOfLine: "lf",
+  arrowParens: "always",
+};
+export default config;
